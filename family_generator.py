1eeac76c68c5540a6cd5edbc655b0d00ee6c427c
Fredi Šarić <fredi.saric.94@gmail.com>
2017-10-14 22:21:52 +0200

    Progress

--- family_generator.py
+++ family_generator.py
@@ -1,88 +1,200 @@
 import numpy as np
 from sklearn import svm
 from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+
+feature_generator_parameters_type_0 = [ # Family
+    # Date time measures mean and std for mean and for std. [0 - 1]
+    0.5,   0.002,     0.002,  0.0009,
+    0.49,  0.00015,   0.0015, 0.00001,
+    0.489, 0.0002,    0.002,  0.002,
+    0.51,  0.00009,   0.0001, 0.0015,
+    0.53,  0.00012,   0.0025, 0.0001,
+    0.52,  0.00001,   0.001,  0.002,
+    0.35,  0,         0.002,  0.0013,
+
+    # Minutes parameters [3, 30 * 60] in seconds
+    2*60,      45,    0.002,  0.0009,
+    3*60,      48,    0.0015, 0.00001,
+    2*60 + 20, 32,    0.002,  0.002,
+    2*60 + 45, 50,    0.0001, 0.0015,
+    3*60 - 10, 28,    0.0025, 0.0001,
+    2*60 + 21, 40,    0.001,  0.002,
+    80,        20,    0.002,  0.0013,
+
+    # Usage type
+    1, 1,
+
+    # Number of location overlapping [0-1] percent of all calls
+    0.3, 0.0015,
+
+    # IOU when calls are not in the same repeater
+    0.2, 0.0001
+]
+
+feature_generator_parameters_type_1 = [ # Friend
+    # Date time measures mean and std for mean and for std. [0 - 1]
+    0.5,   0.002,     0.002,  0.0009,
+    0.49,  0.00015,   0.0015, 0.00001,
+    0.489, 0.0002,    0.002,  0.002,
+    0.51,  0.00009,   0.0001, 0.0015,
+    0.53,  0.00012,   0.0025, 0.0001,
+    0.52,  0.00001,   0.001,  0.002,
+    0.35,  0,         0.002,  0.0013,
+
+    # Minutes parameters [3, 30 * 60] in seconds
+    2*60,      45,    0.002,  0.0009,
+    3*60,      48,    0.0015, 0.00001,
+    2*60 + 20, 32,    0.002,  0.002,
+    2*60 + 45, 50,    0.0001, 0.0015,
+    3*60 - 10, 28,    0.0025, 0.0001,
+    2*60 + 21, 40,    0.001,  0.002,
+    80,        20,    0.002,  0.0013,
+
+    # Usage type
+    1, 1,
+
+    # Number of location overlapping [0-1] percent of all calls
+    0.3, 0.0015,
+
+    # IOU when calls are not in the same repeater
+    0.2, 0.0001
+]
+
+feature_generator_parameters_type_2 = [ # Bussines And Other
+    # Date time measures mean and std for mean and for std. [0 - 1]
+    0.5,   0.002,     0.002,  0.0009,
+    0.49,  0.00015,   0.0015, 0.00001,
+    0.489, 0.0002,    0.002,  0.002,
+    0.51,  0.00009,   0.0001, 0.0015,
+    0.53,  0.00012,   0.0025, 0.0001,
+    0.52,  0.00001,   0.001,  0.002,
+    0.35,  0,         0.002,  0.0013,
+
+    # Minutes parameters [3, 30 * 60] in seconds
+    2*60,      45,    0.002,  0.0009,
+    3*60,      48,    0.0015, 0.00001,
+    2*60 + 20, 32,    0.002,  0.002,
+    2*60 + 45, 50,    0.0001, 0.0015,
+    3*60 - 10, 28,    0.0025, 0.0001,
+    2*60 + 21, 40,    0.001,  0.002,
+    80,        20,    0.002,  0.0013,
+
+    # Usage type
+    1, 1,
+
+    # Number of location overlapping [0-1] percent of all calls
+    0.3, 0.0015,
+
+    # IOU when calls are not in the same repeater
+    0.2, 0.0001
+]
+
+def choose_feature_generator_parameters(type):
+    if type == 0:
+        return feature_generator_parameters_type_0, 1
+    elif type == 1:
+        return feature_generator_parameters_type_1, 0
+    else:
+        return feature_generator_parameters_type_2, 0
 
 def generate_dataset(num_samples, type):
     # Features: Date/time, Usage_type, Measure, service_type, origin_location,
     # receiving_service_type, receiving_location
 
-    if type == 0:
-        # Generate non-family
-        monday = np.abs(12 + np.random.randn(num_samples) * 1.5)
-        tuesday = np.abs(15 + np.random.randn(num_samples) * 0.5)
-        wednesday = np.abs(14 + np.random.randn(num_samples) * 3)
-        thursday = np.abs(17 + np.random.randn(num_samples) * 3.5)
-        friday = np.abs(18 + np.random.randn(num_samples) * 0.1)
-        saturday = np.zeros(num_samples)
-        sunday = np.abs(5 + np.random.randn(num_samples) * 8)
+    params, label = choose_feature_generator_parameters(type)
 
-        monday_measure = np.array(np.abs(20 + np.random.randn(num_samples) * 1),
-                                  dtype=np.int32)
-        tuesday_measure = np.array(np.abs(7 + np.random.randn(num_samples) * 1),
-                                   dtype=np.int32)
-        wednesday_measure = np.array(np.abs(8 + np.random.randn(num_samples) * 1),
-                                     dtype=np.int32)
-        thursday_measure = np.array(np.abs(10 + np.random.randn(num_samples) * 1),
-                                    dtype=np.int32)
-        friday_measure = np.array(np.abs(15 + np.random.randn(num_samples) * 1),
-                                  dtype=np.int32)
-        saturday_measure = np.zeros(num_samples)
-        sunday_measure = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    date_time_features = []
+    for i in range(0, 14):
+        date_time_features.append(np.abs(params[2*i] + np.random.randn(num_samples) * params[2*i+1]))
+    date_time_features = np.vstack(np.array(date_time_features))
 
-        usage_type = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
-        measure = np.array(np.abs(25 + np.random.randn(num_samples) * 4), dtype=np.int32)
-        service_type = np.random.randint(0, 2, (num_samples))
-        origin_location = np.array(np.abs(3 + np.random.randn(num_samples) * 8),
-                                   dtype=np.int32)
-        receiving_type = np.random.randint(0, 2, (num_samples))
-        receiving_location = np.array(np.abs(6 + np.random.randn(num_samples) * 4),
-                                      dtype=np.int32)
-        label = np.ones(num_samples)
-    else:
-        # Generate family
-        monday = np.abs(16 + np.random.randn(num_samples) * 1.5)
-        tuesday = np.abs(11 + np.random.randn(num_samples) * 0.5)
-        wednesday = np.abs(20 + np.random.randn(num_samples) * 3)
-        thursday = np.abs(10 + np.random.randn(num_samples) * 3.5)
-        friday = np.abs(9 + np.random.randn(num_samples) * 0.1)
-        saturday = np.zeros(num_samples)
-        sunday = np.abs(11 + np.random.randn(num_samples) * 8)
+    duration_features = []
+    for i in range(0, 14):
+        duration_features.append(np.abs(params[28 + 2*i] + np.random.randn(num_samples) * params[28 + 2*i + 1]))
+    duration_features = np.vstack(np.array(duration_features))
 
-        monday_measure = np.array(np.abs(40 + np.random.randn(num_samples) * 1),
-                                  dtype=np.int32)
-        tuesday_measure = np.array(np.abs(22 + np.random.randn(num_samples) * 1),
-                                   dtype=np.int32)
-        wednesday_measure = np.array(np.abs(11 + np.random.randn(num_samples) * 1),
-                                     dtype=np.int32)
-        thursday_measure = np.array(np.abs(8 + np.random.randn(num_samples) * 1),
-                                    dtype=np.int32)
-        friday_measure = np.array(np.abs(12 + np.random.randn(num_samples) * 1),
-                                  dtype=np.int32)
-        saturday_measure = np.zeros(num_samples)
-        sunday_measure = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    usage_type = np.array(np.abs(params[28] + np.random.randn(num_samples) * params[29]), dtype=np.int32)
+    location_overlap = np.array(np.abs(params[30] + np.random.randn(num_samples) * params[31]), dtype=np.int32)
+    iou = np.array(np.abs(params[32] + np.random.randn(num_samples) * params[33]), dtype=np.float32)
 
-        usage_type = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
-        measure = np.array(np.abs(50 + np.random.randn(num_samples) * 4), dtype=np.int32)
-        service_type = np.random.randint(0, 2, (num_samples))
-        origin_location = np.array(np.abs(4 + np.random.randn(num_samples) * 8),
-                                   dtype=np.int32)
-        receiving_type = np.random.randint(0, 2, (num_samples))
-        receiving_location = np.array(np.abs(7 + np.random.randn(num_samples) * 4),
-                                      dtype=np.int32)
-        label = np.zeros(num_samples)
+    rest_features = np.vstack((usage_type, location_overlap, iou))
 
-    features = np.vstack((monday, tuesday, wednesday, thursday, friday, saturday, sunday,
-                          monday_measure, tuesday_measure, wednesday_measure,
-                          thursday_measure, friday_measure, saturday_measure, sunday_measure,
-                          usage_type, measure, service_type, origin_location,
-                          receiving_location, receiving_type)).T
-    return features, label
+    features = np.vstack((date_time_features, duration_features, rest_features))
+    labels = np.repeat(label, features.shape[1])
+    return features.T, labels
+
+    #if type == 0:
+    #    # Generate non-family
+    #    monday = np.abs(12 + np.random.randn(num_samples) * 1.5)
+    #    tuesday = np.abs(15 + np.random.randn(num_samples) * 0.5)
+    #    wednesday = np.abs(14 + np.random.randn(num_samples) * 3)
+    #    thursday = np.abs(17 + np.ran0dom.randn(num_samples) * 3.5)
+    #    friday = np.abs(18 + np.random.randn(num_samples) * 0.1)
+    #    saturday = np.zeros(num_samples)
+    #    sunday = np.abs(5 + np.random.randn(num_samples) * 8)
+#
+    #    monday_measure = np.array(np.abs(20 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    tuesday_measure = np.array(np.abs(7 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    wednesday_measure = np.array(np.abs(8 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    thursday_measure = np.array(np.abs(10 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    friday_measure = np.array(np.abs(15 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    saturday_measure = np.zeros(num_samples)
+    #    sunday_measure = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+#
+    #    usage_type = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    measure = np.array(np.abs(25 + np.random.randn(num_samples) * 4),   dtype=np.int32)
+    #    service_type = np.random.randint(0, 2, (num_samples))
+    #    origin_location = np.array(np.abs(3 + np.random.randn(num_samples) * 8), dtype=np.int32)
+    #    receiving_type = np.random.randint(0, 2, (num_samples))
+    #    receiving_location = np.array(np.abs(6 + np.random.randn(num_samples) * 4), dtype=np.int32)
+    #    label = np.ones(num_samples)
+#
+    #else:
+    #    # Generate family
+    #    monday = np.abs(16 + np.random.randn(num_samples) * 1.5)
+    #    tuesday = np.abs(11 + np.random.randn(num_samples) * 0.5)
+    #    wednesday = np.abs(20 + np.random.randn(num_samples) * 3)
+    #    thursday = np.abs(10 + np.random.randn(num_samples) * 3.5)
+    #    friday = np.abs(9 + np.random.randn(num_samples) * 0.1)
+    #    saturday = np.zeros(num_samples)
+    #    sunday = np.abs(11 + np.random.randn(num_samples) * 8)
+#
+    #    monday_measure = np.array(np.abs(40 + np.random.randn(num_samples) * 1),
+    #                              dtype=np.int32)
+    #    tuesday_measure = np.array(np.abs(22 + np.random.randn(num_samples) * 1),
+    #                               dtype=np.int32)
+    #    wednesday_measure = np.array(np.abs(11 + np.random.randn(num_samples) * 1),
+    #                                 dtype=np.int32)
+    #    thursday_measure = np.array(np.abs(8 + np.random.randn(num_samples) * 1),
+    #                                dtype=np.int32)
+    #    friday_measure = np.array(np.abs(12 + np.random.randn(num_samples) * 1),
+    #                              dtype=np.int32)
+    #    saturday_measure = np.zeros(num_samples)
+    #    sunday_measure = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+#
+    #    usage_type = np.array(np.abs(1 + np.random.randn(num_samples) * 1), dtype=np.int32)
+    #    measure = np.array(np.abs(50 + np.random.randn(num_samples) * 4), dtype=np.int32)
+    #    service_type = np.random.randint(0, 2, (num_samples))
+    #    origin_location = np.array(np.abs(4 + np.random.randn(num_samples) * 8),
+    #                               dtype=np.int32)
+    #    receiving_type = np.random.randint(0, 2, (num_samples))
+    #    receiving_location = np.array(np.abs(7 + np.random.randn(num_samples) * 4),
+    #                                  dtype=np.int32)
+    #    label = np.zeros(num_samples)
+#
+    #features = np.vstack((monday, tuesday, wednesday, thursday, friday, saturday, sunday,
+    #                      monday_measure, tuesday_measure, wednesday_measure,
+    #                      thursday_measure, friday_measure, saturday_measure, sunday_measure,
+    #                      usage_type, measure, service_type, origin_location,
+    #                      receiving_location, receiving_type)).T
+    #return features, label
 
 
-family, fam_labels = generate_dataset(500, 0)
-non_family, labels = generate_dataset(600, 1)
+family, fam_labels = generate_dataset(10, 0)
+non_family, labels = generate_dataset(10, 1)
 
 x = np.vstack((family, non_family))
 y = np.hstack((fam_labels, labels))
@@ -91,4 +203,5 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 clf = svm.SVC()
 clf.fit(x_train, y_train)
 
+print(accuracy_score(y_test, clf.predict(x_test)))
 print(f1_score(y_test, clf.predict(x_test)))
